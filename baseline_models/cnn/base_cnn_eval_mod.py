285a89fc6665f1aed645497cf5929ea5577edfd7
arch13m <99214961+arch13m@users.noreply.github.com>
2025-09-03 10:53:53 +0100

    Create base_cnn_eval_mod.py

---
+++ baseline_models/cnn/base_cnn_eval_mod.py
@@ -0,0 +1,20 @@
+model = CNN_BASE(LATENT_DIM)
+model.load_state_dict(torch.load('CNN_BASE.pth'))
+
+model.eval()
+modal_pitch_all = []
+alpha_all       = []
+for alpha in np.arange(-20, 20, 4):
+  modal_pitch = cnn_vary_constrained_latent_dim(1, alpha, attribute="mod")
+  modal_pitch_all.append(modal_pitch)
+  alpha_all.append(alpha)
+
+plt.plot(alpha_all, modal_pitch_all, marker = ".")
+current_values = plt.gca().get_yticks()
+plt.gca().set_yticks(current_values)
+plt.gca().set_yticklabels(pitch_num_to_name(int(x)) for x in current_values)
+plt.xlabel('Alpha')
+plt.ylabel('Modal pitch')
+plt.show()
+
+cnn_interpretability_metric(attribute="mod")
