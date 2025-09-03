1482be31839bc9ed1246b7d1654d2f851b2bb483
arch13m <99214961+arch13m@users.noreply.github.com>
2025-09-03 10:54:33 +0100

    Create base_cnn_eval_rhy.py

---
+++ baseline_models/cnn/base_cnn_eval_rhy.py
@@ -0,0 +1,20 @@
+model = CNN_BASE(LATENT_DIM)
+model.load_state_dict(torch.load('CNN_BASE.pth'))
+
+model.eval()
+rhythmic_complexity_all = []
+alpha_all               = []
+for alpha in np.arange(-20, 10, 1):
+  rhythmic_complexity = cnn_vary_constrained_latent_dim(1, alpha, attribute="rhy")
+  rhythmic_complexity_all.append(rhythmic_complexity)
+  alpha_all.append(alpha)
+
+plt.plot(alpha_all, rhythmic_complexity_all, 'red', marker = ".")
+current_values = plt.gca().get_yticks()
+plt.gca().set_yticks(current_values)
+plt.gca().set_yticklabels(round(x,2) for x in current_values)
+plt.xlabel('Alpha')
+plt.ylabel('Rhythmic complexity')
+plt.show()
+
+cnn_interpretability_metric(attribute="rhy")
